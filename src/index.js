da60bea276bc8df8a90fa6351ee68685ff3c2a58
hungrated <zhang295415658@qq.com>
2019-05-15 17:13:59 +0800

    master: init & base feature

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const app = require("express")();
+const http = require("http").Server(app);
+const io = require("socket.io")(http);
+const open = require("open");
+
+const PORT = 9001;
+
+app.get("/", function(req, res) {
+  res.sendFile(`${__dirname}/public/index.html`);
+});
+
+io.on("connection", function(socket) {
+  console.log("A user connected.");
+
+  socket.on("message", function (msg) {
+    io.emit("message", msg) //将新消息广播出去
+  })
+});
+
+http.listen(PORT, function() {
+  const url = `http://127.0.0.1:${PORT}`;
+  console.log(`Soul chatter activated: ${url}`);
+  open(url);
+});
